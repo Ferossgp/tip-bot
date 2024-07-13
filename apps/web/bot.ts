5d0b26d965141f94b5c800baa907b4d964605133
Gheorghe Pinzaru <feross95@gmail.com>
2024-07-13 11:56:28 +0200

    add database

--- apps/web/bot.ts
+++ apps/web/bot.ts
@@ -1,7 +1,36 @@
 import 'dotenv/config'
 import TelegramBot from "node-telegram-bot-api";
+import SQLite from 'better-sqlite3'
+import {
+  getInstanceInfoSync,
+} from 'litefs-js'
+import { remember } from '@epic-web/remember';
 
 const token = process.env.TELEGRAM_BOT_TOKEN;
+const DATABASE_PATH = process.env.DATABASE_PATH
+
+function createDatabase() {
+  const db = new SQLite(DATABASE_PATH)
+
+  const { currentIsPrimary } = getInstanceInfoSync()
+  if (!currentIsPrimary) return db
+
+  try {
+    // TODO: create tables
+    db.exec(`
+			CREATE TABLE IF NOT EXISTS test (
+				key TEXT PRIMARY KEY,
+				value TEXT
+			)
+		`)
+  } catch (error: unknown) {
+    throw error
+  }
+
+  return db
+}
+
+export const db = remember('db', createDatabase)
 
 if (!token) {
   throw new Error('TELEGRAM_BOT_TOKEN must be provided!');
